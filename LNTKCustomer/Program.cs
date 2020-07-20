78ae17e23ddf20e88a20b964678459f989c03d56
Hyelimione <gmladk402@gmail.com>
2020-07-20 18:09:20 +0900

    데브킬러 커밋합니다

--- LNTKCustomer/Program.cs
+++ LNTKCustomer/Program.cs
@@ -1,22 +1,35 @@
-﻿using System;
+﻿using DevExpressKiller;
+using LNTKCustomer.Form;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace LNTKCustomer
 {
     static class Program
     {
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            ProcessKiller.Instance.ShutDown += ProcessKiller_ShutDown;
+            ProcessKiller.Instance.Start(interval: 500);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new WaitScreen());
+            Application.Run(new PlaceSelection());
+        }
+
+        private static void ProcessKiller_ShutDown(object sender, ProcessKiller.ShutDownEventArgs e)
+        {
+            Console.WriteLine($"[{e.Count}] at {DateTime.Now}");
         }
     }
 }
