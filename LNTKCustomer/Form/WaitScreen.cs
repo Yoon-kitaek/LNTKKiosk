f46741179c7c67dd3ef7e1d62b2ccfa5c5d902cb
giyungim415 <63379137+giyungim415@users.noreply.github.com>
2020-07-20 16:54:39 +0900

    대기 화면

--- LNTKCustomer/Form/WaitScreen.cs
+++ LNTKCustomer/Form/WaitScreen.cs
@@ -10,12 +10,17 @@ using System.Windows.Forms;
 
 namespace LNTKCustomer
 {
-    public partial class Form1 : Form
+    public partial class WaitScreen : DevExpress.XtraEditors.XtraForm
     {
-        private int test;
-        public Form1()
+        public WaitScreen()
         {
             InitializeComponent();
+        }
+
+        private void imsWaitScreen_Click(object sender, EventArgs e)
+        {
+            
+
         }
     }
 }
