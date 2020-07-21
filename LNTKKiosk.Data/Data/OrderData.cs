149b3f35d9bf3db5692b22e2a9d35c909cfa2ab4
신형 최 <hemelic@naver.com>
2020-07-21 09:36:47 +0900

    aaa

---
+++ LNTKKiosk.Data/Data/OrderData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNTKKiosk.Data
+{
+    public class OrderData : EntityData<Order>
+    {
+    }
+}
