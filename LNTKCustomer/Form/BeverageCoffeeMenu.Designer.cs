8f4ef0673e2a084c86722f1dd63335806113039b
Hyelimione <gmladk402@gmail.com>
2020-07-22 15:04:13 +0900

    ㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂ

--- LNTKCustomer/Form/BeverageCoffeeMenu.Designer.cs
+++ LNTKCustomer/Form/BeverageCoffeeMenu.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
             this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
-            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.thumbnail1 = new LNTKCustomer.UserControl.Thumbnail();
-            this.thumbnail2 = new LNTKCustomer.UserControl.Thumbnail();
             this.thumbnail3 = new LNTKCustomer.UserControl.Thumbnail();
+            this.thumbnail2 = new LNTKCustomer.UserControl.Thumbnail();
+            this.thumbnail1 = new LNTKCustomer.UserControl.Thumbnail();
             this.thumbnail4 = new LNTKCustomer.UserControl.Thumbnail();
             this.thumbnail5 = new LNTKCustomer.UserControl.Thumbnail();
             this.thumbnail6 = new LNTKCustomer.UserControl.Thumbnail();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
             this.splitContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,16 +56,6 @@
             this.layoutControl1.Size = new System.Drawing.Size(1016, 538);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
-            // 
-            // Root
-            // 
-            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
-            this.Root.GroupBordersVisible = false;
-            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem1});
-            this.Root.Name = "Root";
-            this.Root.Size = new System.Drawing.Size(1016, 538);
-            this.Root.TextVisible = false;
             // 
             // splitContainerControl1
             // 
@@ -84,22 +74,13 @@
             this.splitContainerControl1.SplitterPosition = 254;
             this.splitContainerControl1.TabIndex = 0;
             // 
-            // layoutControlItem1
+            // thumbnail3
             // 
-            this.layoutControlItem1.Control = this.splitContainerControl1;
-            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(996, 518);
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem1.TextVisible = false;
-            // 
-            // thumbnail1
-            // 
-            this.thumbnail1.Label = "label";
-            this.thumbnail1.Location = new System.Drawing.Point(3, 3);
-            this.thumbnail1.Name = "thumbnail1";
-            this.thumbnail1.Size = new System.Drawing.Size(322, 247);
-            this.thumbnail1.TabIndex = 0;
+            this.thumbnail3.Label = "label";
+            this.thumbnail3.Location = new System.Drawing.Point(663, 4);
+            this.thumbnail3.Name = "thumbnail3";
+            this.thumbnail3.Size = new System.Drawing.Size(322, 247);
+            this.thumbnail3.TabIndex = 2;
             // 
             // thumbnail2
             // 
@@ -109,13 +90,13 @@
             this.thumbnail2.Size = new System.Drawing.Size(322, 247);
             this.thumbnail2.TabIndex = 1;
             // 
-            // thumbnail3
+            // thumbnail1
             // 
-            this.thumbnail3.Label = "label";
-            this.thumbnail3.Location = new System.Drawing.Point(663, 4);
-            this.thumbnail3.Name = "thumbnail3";
-            this.thumbnail3.Size = new System.Drawing.Size(322, 247);
-            this.thumbnail3.TabIndex = 2;
+            this.thumbnail1.Label = "label";
+            this.thumbnail1.Location = new System.Drawing.Point(3, 3);
+            this.thumbnail1.Name = "thumbnail1";
+            this.thumbnail1.Size = new System.Drawing.Size(322, 247);
+            this.thumbnail1.TabIndex = 0;
             // 
             // thumbnail4
             // 
@@ -141,6 +122,25 @@
             this.thumbnail6.Size = new System.Drawing.Size(322, 247);
             this.thumbnail6.TabIndex = 3;
             // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(1016, 538);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.splitContainerControl1;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(996, 518);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
             // BeverageCoffeeMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
@@ -151,9 +151,9 @@
             this.Text = "BeverageCoffeeMenu";
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
             this.splitContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
             this.ResumeLayout(false);
 
--- LNTKCustomer/Form/BurgerSetMenu.Designer.cs
+++ LNTKCustomer/Form/BurgerSetMenu.Designer.cs
@@ -28,50 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
-            this.splitContainerControl1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(159, 70);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 18);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "리뉴얼 중입니다(버거 메뉴란입니다)";
-            // 
-            // splitContainerControl1
-            // 
-            this.splitContainerControl1.Location = new System.Drawing.Point(12, 12);
-            this.splitContainerControl1.Name = "splitContainerControl1";
-            this.splitContainerControl1.Panel1.Text = "Panel1";
-            this.splitContainerControl1.Panel2.Text = "Panel2";
-            this.splitContainerControl1.Size = new System.Drawing.Size(776, 426);
-            this.splitContainerControl1.TabIndex = 3;
             // 
             // BurgerSetMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.splitContainerControl1);
-            this.Controls.Add(this.label1);
             this.Name = "BurgerSetMenu";
             this.Text = "BurgerSetMenu";
             this.Load += new System.EventHandler(this.BurgerSetMenu_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
-            this.splitContainerControl1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
     }
 }
